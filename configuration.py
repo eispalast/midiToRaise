fdf49d68db362504b9db3dc1a03ffd8ca47c816c
Timo Schlegel <timo.schlegel@campus.tu-berlin.de>
2022-05-09 12:15:18 +0200

    assign new bindings from within the prog

--- configuration.py
+++ configuration.py
@@ -1,12 +1,13 @@
 import json
-from unittest import result
-
-from boto import config
+import time
+from tracemalloc import start
 from menu import Menu
 from pygame import midi as midi
 
 MIDIEVENTS_I2S = {0x8:"Note Off", 0x9:"Note On", 0xC:"PC"}
 MIDIEVENTS_S2I = {"Note Off":0x8, "Note On":0x9, "PC":0xC}
+
+ACTION_I2S = {0:"activate",1:"deactivate",2:"moveTo"}
 
 class configuration:
     path = None
@@ -18,6 +19,7 @@ class configuration:
         self.path = path
         self.readConfig()
         self.initMidi()
+        self.config_wait_for_midi_timeout = 2
     
     @property
     def midi_device_name(self):
@@ -82,8 +84,72 @@ class configuration:
     def optionsMenu(self):
         pass
 
-    def assignNewMenu(self):
+    def selectMIDIinput(self):
         print("Press any midi key")
+        starttime = time.time()
+        midiinputs = []
+        while time.time()-starttime < self.config_wait_for_midi_timeout:
+            if midi.Input.poll(self.midi_device):
+                ins = midi.Input.read(self.midi_device,3)
+                for i in ins:
+                    midiinputs.append(i[0])
+        print(midiinputs)
+        options = []
+        translated_inputs = []
+        for id, m in enumerate(midiinputs):
+            channel = m[0] & 0xF
+            event = MIDIEVENTS_I2S[m[0]>>4]
+            key = m[1]
+            options.append(f"[{id}] Ch. {channel} {event} on {key}")
+            translated_inputs.append([channel,event,key])
+        options.append("[c] cancel")
+        menu = Menu(options,"Select a MIDI event")
+        result = menu.start()
+        if result == (len(options)-1):
+            return None
+        
+        return translated_inputs[result]
+    
+    def selectLayerAction(self, midiinput = "this midi input"):
+        action_options = ["[a] activate layer", "[d] deactivate layer", "[m] moveTo layer", "[c] cancel"]
+        menu = Menu(action_options, f"Select an action for {midiinput}")
+        action_result = menu.start()
+        if action_result == (len(action_options)-1):
+            return None
+
+        layer_options = list([f"[{x}] Layer {x}" for x in range(10)])
+        layer_results = []
+        while True:
+            menu = Menu(layer_options,"Chose a corresponding layer")
+            layer_results.append(menu.start())
+            if action_result != 2:
+                break
+            menu = Menu(["[y] yes", "[n] no"],"Do you want to add more layers to cycle through?")
+            more_result = menu.start()
+            if more_result == 1:
+                break
+        
+        return [action_result,layer_results]
+        
+
+    def assignNewMenu(self):
+        midiinput = self.selectMIDIinput()
+        if midiinput == None:
+            return
+        action = self.selectLayerAction(midiinput=midiinput)
+        if action == None:
+            return
+        
+        self.assignments.append({"midi":{"channel":midiinput[0],"event":MIDIEVENTS_S2I[midiinput[1]],"key":midiinput[2]},"raise_action":{"action":ACTION_I2S[action[0]],"layers":action[1]}})
+
+        # in case it was a note on event and the action was activate, offer to automatically configure the deactivate function
+        if midiinput[1] == 'Note On' and action[0] == 0:
+            menu = Menu(["[y] yes", "[n] no"], "Do you want to automatically assign the corresponding deactivate layer action?")
+            result = menu.start()
+            if result == 0:
+                self.assignments.append({"midi":{"channel":midiinput[0],"event":8,"key":midiinput[2]},"raise_action":{"action":ACTION_I2S[1],"layers":action[1]}})
+        
+        self.writeConfig()
     
     def editMenu(self):
         # first layer edit menu
@@ -92,7 +158,7 @@ class configuration:
         for id, a in enumerate(self.assignments):
             midi = a["midi"]
             raise_action = a["raise_action"]
-            options.append(f"[{id}] {(str(midi['channel'])) :3} {midi['event']:8} {(str(midi['key'])):3} -> {raise_action['action']} {raise_action['layers']}")
+            options.append(f"[{id}] {(str(midi['channel'])) :3} {MIDIEVENTS_I2S[midi['event']]:8} {(str(midi['key'])):3} -> {raise_action['action']} {raise_action['layers']}")
         options.append("[b] back")
         
         menu = Menu(options,title="Choose an assignment to edit")
